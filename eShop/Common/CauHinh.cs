8534ab18d8875ad4ca8027f7f760cd22036d7246
nhatphidev <honhatphi44@gmail.com>
2024-05-03 21:44:45 +0700

    Thêm trang quản lý loại sản phẩm và sản phẩm dành cho admin

--- eShop/Common/CauHinh.cs
+++ eShop/Common/CauHinh.cs
@@ -5,5 +5,5 @@ public class CauHinh
     public const int ChieuRongSanPham = 160;
     public const int ChieuCaoSanPham = 250;
     public const string DuongDanThuMucAnh = "D:/Code/eShop/eShop/Images/";
-    public const string AnhMacDinh = "default.png";
+    public const string AnhMacDinh = "default.jpg";
 }
